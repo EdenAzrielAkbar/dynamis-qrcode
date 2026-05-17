70962d29f0e22a8013b77b1d279a88ada0655957
edenazrielakbar-cloud <edenazrielakbar@gmail.com>
2026-05-17 07:36:10 +0700

    memperbaiki configurasi di main.mjs

--- controllers/authController.mjs
+++ controllers/authController.mjs
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: process.env.CALLBACK_URL,
     },
     async (accesToken, refreshToken, profile, done) => {
       try {
